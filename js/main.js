e8e9194c1cbb36183428ce2d5fcd444fc83ab0c8
Scorpion96522 <126128140+Scorpion96522@users.noreply.github.com>
2023-03-29 10:32:23 +0300

    finction date

--- js/main.js
+++ js/main.js
@@ -31,9 +31,37 @@ window.onload = function() {
         let hours = date.getHours();
         let minutes = date.getMinutes();
         let seconds = date.getSeconds();
+        let Year = date.getFullYear();
+        let month = (date.getMonth() + 1);
+        let day = date.getDay();
 
+
+        if (hours < 10) hours = "0" + hours;
+        if (minutes < 10) minutes = "0" + minutes;
+        if (seconds < 10) seconds = "0" + seconds;
         let clock = hours + ":" + minutes + ":" + seconds;
+        let fullDate = day + ", " + month + ", " + Year;
         document.getElementById("clock").innerHTML = clock;
-    }
-    );
+        document.getElementById("DATE").innerHTML = fullDate;
+    }, 1000);
+};
+
+// function addLeadingZero(date) {
+//     return(date < 10 ) ? "0" + date : date;
+// }
+
+const date = new Date();
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getDay());
+
+function ustime(t) {
+    let Y = t.getFullYear();
+    let M = (t.getMonth() + 1);
+    let D = t.getDate();
+
+    console.log(Y, M, D);
 }
+
+console.log(ustime)
