52efe43c36d760e58a02ce87b2afed9d1cd7feae
Scorpion96522 <126128140+Scorpion96522@users.noreply.github.com>
2023-04-01 13:36:36 +0300

    fixed am/pm

--- js/main.js
+++ js/main.js
@@ -8,15 +8,28 @@ function updateClock() {
         hou = now.getHours(),
         min = now.getMinutes(),
         sec = now.getSeconds(),
-        pe = "AM";
+        pe = "PM";
 
-        if (hou == 0){
-            hou = 12;
-        }
+        // if (hou == 0){
+        //     hou = 12;
+        // }
+        // if (hou > 12){
+        //     hou = hou -12;
+        //     pe = "PM";
+        // }
+
         if (hou > 12){
-            hou = hou -12;
-            pe = "PM";
+            hou = hou;
         }
+        if  (hou < 12) {
+            hou = hou;        
+            pe = "AM";
+        }
+        // if (hou == 12){
+        //     hou > 12;
+        //     pe = "PM";
+        // }
+
 
         Number.prototype.pad = function(digits){
             for(var n = this.toString(); n.length < digits; n = 0 + n);
